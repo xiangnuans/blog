3c32a44af8567107fa1ad2b87c099d667eb6b96c
guoyanhong <guoyanhong@cnstrong.cn>
2021-12-25 20:21:36 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env sh
 
-set -e
+# 自动部署脚本
+yourname="coco723"
 
-vuepress build
+# 构建
+npm run build
+# 保存历史commit记录，否则每次commit之后只有一条commit信息
+git clone git@github.com:${yourname}/${yourname}.github.io.git .temp
+mv .temp/.git dist
+rm -rf .temp
+# 导航到构建输出目录
+cd dist
 
-cd docs/.vuepress/dist
+# 发布到自定义域名
+echo '${yourname}.github.io' > CNAME
 
+git add -A
+git commit -m 'deploy'
 
-git init
-git remote add origin https://${REF}
-git add .
-git commit -m "Updated By Deploy With Build For Github Pages"
-git push --force --quiet "https://${TOKEN}@${REF}" master:gh-pages
-
-cd -
+# 推到仓库的 master 分支
+git push -f git@github.com:${yourname}/${yourname}.github.io.git master
