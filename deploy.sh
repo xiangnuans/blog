3a995e0f527815d7dbd4b8f7aafc463ecb71e749
guoyanhong <guoyanhong@cnstrong.cn>
2021-12-25 20:15:46 +0800

    chore: deploy

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,16 @@
 #!/usr/bin/env sh
 
-# 确保脚本抛出遇到的错误
 set -e
 
-# 生成静态文件
-npm run docs:build
+vuepress build
 
-# 进入静态文件的文件夹
 cd docs/.vuepress/dist
 
-# 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
 
 git init
-git add -A
-git commit -m 'deploy'
-
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-# git push origin 本地分支:远端希望创建的分支
-git push -f git@github.com:<coco723>/blog.git master:gh-pages
+git remote add origin https://${REF}
+git add .
+git commit -m "Updated By Deploy With Build For Github Pages"
+git push --force --quiet "https://${TOKEN}@${REF}" master:gh-pages
 
 cd -
