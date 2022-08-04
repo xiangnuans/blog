72682b038cec860f24a9ec343452ca27b65ae6d3
coco723 <352118502@qq.com>
2022-08-04 14:26:12 +0800

    refactor: 重构博客框架设计

--- docs/.vuepress/configs/sidebar.js
+++ docs/.vuepress/configs/sidebar.js
@@ -8,14 +8,6 @@ module.exports = [
             // ["js/scope", "理解Js中的作用域-作用域链以及闭包"]
         ]
     }
-    // {
-    //     title: "工具",
-    //     collapsable: true,
-    //     children: [
-    //       ["tools/", "目录"],
-    //       ["tools/vuepress-build-blog", "手把手教你用vuepress搭建博客"]
-    //     ]
-    // }
 ]
 // {
 //     title: "React进阶",
